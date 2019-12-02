16db85451c1041a389ab9b2c67405f5ee9c2e33e
emilindadie <dadie.emilin@gmail.com>
2019-12-02 13:44:17 +0100

    operation service: should valid all create operation information OK case

--- src/models/operation/createOperation.tsx
+++ src/models/operation/createOperation.tsx
@@ -1,4 +1,11 @@
 export class CreateOperation {
     accountId!: number;
     amount!: number;
+
+    validProperty(): boolean {
+        if (this.accountId && this.amount) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/services/operation.tsx
+++ src/services/operation.tsx
@@ -2,7 +2,7 @@ import { CreateOperation } from '../models/operation/createOperation';
 
 const OperationService = {
     validCreateOperationInformation(operation: CreateOperation) {
-        return '';
+        return operation.validProperty();
     },
 };
 
